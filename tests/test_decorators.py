3764eae701ff45386dd7013f9f5db5a861f19fc4
Amyrski <vivisnext007786@gmail.com>
2024-12-22 19:38:18 +1000

    add tests for decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,60 +1,62 @@
+import pytest
+
 from src.decorators import log
 
 
-def test_log_valid_data_with_file(tmp_path):
-    """Тестирование записи в файл с переданным файлом."""
+def test_log():
+    """Тестироване работы декорированной функции"""
 
-    @log(tmp_path / "my_log.txt")
-    def func_with_file(x, y):
+    @log(filename="")
+    def my_function(x, y):
         return x + y
 
-    result = func_with_file(3, 4)
-    assert result == 7
-
-    """Проверяем, что сообщение было записано в файл"""
-    with open(tmp_path / "my_log.txt", "r") as f:
-        assert f.read() == "my_function ok\n"
-
-
-def test_log_valid_data_without_file(capsys):
-    """Тестирование вывода в консоль без переданного файла."""
-
-    @log()
-    def func_without_file(x, y):
-        return x + y
-
-    result = func_without_file(1, 2)
+    result = my_function(1, 2)
     assert result == 3
-    """Проверяем, что результат и сообщение "my_function ok" было выведено в консоль"""
-    captured = capsys.readouterr()
-    assert captured.out == "my_function ok\n\n"
 
 
-def test_log_invalid_data_with_file(tmp_path):
-    """Тестирование записи в файл с переданным файлом."""
+def test_log_successful(capsys):
+    """Тестирование вывода об успешном фыполнении функции в консоль"""
 
-    @log(tmp_path / "my_log.txt")
-    def func_with_file(x, y):
+    @log(filename="")
+    def my_function(x, y):
         return x + y
 
-    func_with_file(3, "a")
-
-    """Проверяем, что сообщение было записано в файл"""
-    with open(tmp_path / "my_log.txt", "r") as f:
-        assert (
-            f.read() == "my_function error: unsupported operand type(s) for +: 'int' and 'str'. Inputs: (3, 'a'), {}\n"
-        )
+    my_function(1, 2)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function ok\n"
 
 
-def test_log_invalid_data_without_file(capsys):
-    """Тестирование вывода в консоль без переданного файла."""
+def test_log_exception(capsys):
+    """Тестирование вывода об ошибке функции в консоль"""
 
-    @log()
-    def func_without_file(x, y):
+    @log(filename="")
+    def my_function(x, y):
         return x + y
 
-    func_without_file("$", 1)
-
-    """Проверяем, что  сообщение  было выведено в консоль"""
+    my_function(1, "2")
     captured = capsys.readouterr()
-    assert captured.out == "my_function error: can only concatenate str (not \"int\") to str. Inputs: ('$', 1), {}\n\n"
+    assert captured.out == "my_function error: TypeError. Inputs: (1, '2'), {}\n"
+
+
+def test_log_in_file(capsys):
+    """Тестирование вывода об успешном выполнении функции в файл"""
+
+    @log(filename="mylog.txt")
+    def my_function(x, y):
+        return x + y
+
+    my_function(1, 2)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function ok\n"
+
+
+def test_log_in_file_exception(capsys):
+    """Тестирование вывода об ошибке фыполнении функции в файл"""
+
+    @log(filename="mylog.txt")
+    def my_function(x, y):
+        return x + y
+
+    my_function(1, "2")
+    captured = capsys.readouterr()
+    assert captured.out == "my_function error: TypeError. Inputs: (1, '2'), {}\n"
