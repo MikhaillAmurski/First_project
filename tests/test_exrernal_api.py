87753012c5ad59b3726fd9604e6a42c4621e958d
Amyrski <vivisnext007786@gmail.com>
2024-12-22 19:39:47 +1000

    add tests for exrernal_api

--- tests/test_exrernal_api.py
+++ tests/test_exrernal_api.py
@@ -1,37 +1,32 @@
-import unittest
 from unittest.mock import patch
-from src.external_api import amount_transaction
+
+import pytest
+
+from src.external_api import currency_conversion
 
 
-@patch("requests.request")
-def test_amount_transaction_rub(mock_request):
-    """Проверяем, что транзакция в рублях корректно обрабатывается"""
-    mock_request.return_value.json.return_value = {"result": 31957.58}
-    transaction = {
+@pytest.fixture
+def trans_1():
+    return {
         "id": 441945886,
         "state": "EXECUTED",
         "date": "2019-08-26T10:50:58.294041",
         "operationAmount": {
             "amount": "31957.58",
-            "currency": {"name": "руб.", "code": "RUB"},
+            "currency": {
+                "name": "руб.",
+                "code": "USD"}
         },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
     }
-    result = amount_transaction(transaction)
-    assert result == "31957.58"
 
 
-@patch("requests.request")
-def test_amount_transaction_other_currency(mock_request):
-    """Проверяем, что транзакция в других валютах корректно обрабатывается"""
-    mock_request.return_value.json.return_value = {"result": 704216.961075}
-    transaction = {
-        "id": 441945886,
-        "state": "EXECUTED",
-        "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {
-            "amount": "8221.37",
-            "currency": {"name": "USD.", "code": "USD"},
-        },
-    }
-    result = amount_transaction(transaction)
-    assert result == 704216.961075
+@patch('requests.get')
+def test_currency_conversion(mock_get, trans_1):
+    mock_get.return_value.json.return_value = {'success': True, 'query':
+                                               {'from': 'USD', 'to': 'RUB', 'amount': 8221.37}, 'info':
+                                               {'timestamp': 1724671757, 'rate': 91.475458},
+                                               'date': '2024-08-26', 'result': 752053.586137}
+    assert currency_conversion(trans_1) == 752053.586137
