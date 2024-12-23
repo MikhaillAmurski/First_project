c8c56c0a9ef9616d3d15a4fccb5062bf0c0131a6
Amyrski <vivisnext007786@gmail.com>
2024-12-23 23:36:28 +1000

    correct rework_3

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,11 +1,13 @@
-import pytest
-import json
 from datetime import datetime
 
-from src.services import cashback_categories
+from src.services import investment_bank
 
-def test_cashback_categories(cashback):
 
-    expected = json.dumps({'Супермаркеты': 24.0, 'Фастфуд': 3.0}, ensure_ascii=False)
-    result = cashback_categories(cashback, 2021, 11)
-    assert result == expected
+def test_investment_bank(trans_for_services):
+    date_obj = datetime(2021, 12, 31)
+    str_date = datetime.strftime(date_obj, "%Y-%m")
+    assert investment_bank(str_date, trans_for_services, 50) == "220500.0"
+    date_obj = datetime(2021, 5, 31)
+    str_date = datetime.strftime(date_obj, "%Y-%m")
+    assert investment_bank(str_date, trans_for_services, 50) == 0
+
